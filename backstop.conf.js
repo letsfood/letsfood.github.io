4e7f0dd411d568c0fbeb42ad1d190cd9451c3c0a
CJ Patoilo <cjpatoilo@gmail.com>
2017-12-03 17:24:01 -0300

    Huge improvement

--- backstop.conf.js
+++ backstop.conf.js
@@ -1,30 +1,46 @@
-module.exports = {
+const config = {
 	viewports: [
-		{ name: 'phone', width: 320, height: 480 },
-		{ name: 'laptop', width: 1280, height: 800 }
+		{
+			name: 'phone',
+			width: 320,
+			height: 480
+		},
+		{
+			name: 'laptop',
+			width: 1280,
+			height: 800
+		}
 	],
-	scenarios: [{
-		label: 'Home',
-		url: 'http://localhost:3000',
-		hideSelectors: [],
-		removeSelectors: [],
-		selectors: ['body'],
-		readyEvent: null,
-		delay: 2000,
-		misMatchThreshold: 0.1,
-		onReadyScript: null,
-		onBeforeScript: null
-	}],
+	scenarios: [
+		{
+			label: 'Home',
+			url: 'http://localhost:3000',
+			hideSelectors: [],
+			removeSelectors: [],
+			selectors: ['body'],
+			readyEvent: null,
+			delay: 2000,
+			misMatchThreshold: 0.1,
+			onReadyScript: null,
+			onBeforeScript: null
+		}
+	],
 	paths: {
 		bitmaps_reference: 'test/regression',
-		bitmaps_test: 'node_modules/backstopjs/.tmp/bitmaps_test',
-		casper_scripts: 'node_modules/backstopjs/.tmp/casper_scripts',
-		html_report: 'node_modules/backstopjs/.tmp/html_report',
-		ci_report: 'node_modules/backstopjs/.tmp/ci_report'
+		bitmaps_test: 'node_modules/.cache/bitmaps_test',
+		casper_scripts: 'node_modules/.cache/casper_scripts',
+		html_report: 'node_modules/.cache/html_report',
+		ci_report: 'node_modules/.cache/ci_report'
 	},
 	engine: 'phantomjs',
-	report: ['browser', 'CLI', 'CI'],
+	report: [
+		'browser',
+		'CLI',
+		'CI'
+	],
 	casperFlags: [],
 	debug: false,
 	port: 3002
 }
+
+module.exports = config
